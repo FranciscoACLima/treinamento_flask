93d4a3789155fa6d6a25e8fab4e95fb5395d1e49
FranciscoACLima <franciscoaclima@outlook.com>
2019-12-29 11:10:54 -0300

    6. Página de Perfil e Avatares

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Usuário', validators=[DataRequired()])
+    about_me = TextAreaField('Sobre mim', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Gravar')
 
 
 class LoginForm(FlaskForm):
