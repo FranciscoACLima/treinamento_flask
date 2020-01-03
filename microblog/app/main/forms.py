591227b6823421e026ec319e199b9f26c2298923
FranciscoACLima <sE270466**>
2020-01-03 20:38:40 -0300

    16. Pesquisas com o mecanismo FullText

--- microblog/app/main/forms.py
+++ microblog/app/main/forms.py
@@ -1,9 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField
+from wtforms import SubmitField, TextAreaField, StringField
 from wtforms.validators import DataRequired, Length
+from flask import request
 
 
 class PostForm(FlaskForm):
     post = TextAreaField('Diga algo', validators=[
         DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Gravar')
+
+
+class SearchForm(FlaskForm):
+    q = StringField('Buscar', validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
